e184986960a1146540d4749486f98241d79d8ac0
Anas Al-Sweity <123195588+aalsweity@users.noreply.github.com>
2026-06-22 18:21:24 -0400

    Time: 0 ms (100.00%) | Memory: 8.9 MB (67.30%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,3 +1,4 @@
+// this solution took me 4H 12M 42S to solve. First time with stacks in years.
 class Solution 
 {
 public:
@@ -14,35 +15,43 @@ public:
         // while you are at it, check if there are valid
         for(int i = 0; i < s.length(); i++)
         {   
+            // check if a valid starter is up and push it on the stack if true
             if(s[i] == '(' || s[i] == '{' || s[i] == '[')
             {
                 pstacks.push(s[i]);
             }
 
+            // in cases not a starter opener
             else
             {
+                // although we protected from a odd match, still can get empty at some point.
                 if(pstacks.empty())
                 {
                     return false;
                 }
-
+                
+                // this will remove an occurence if valid
                 if(pstacks.top() == '(' && s[i] == ')' || pstacks.top() == '{' && s[i] == '}' || pstacks.top() == '[' && s[i] == ']')
                 {
                     pstacks.pop();
                 }
 
+                // in case of a mismatch occuring
                 else
-                {
+                {   
+                    // end the game early
                     return false;
                 }
 
             }
             
         }    
+        // if everything went smoothly and nothing to match
         if(pstacks.empty())
         {
             return true;
         }
+        // you got leftovers and no matches for them
         else
         {
             return false;
