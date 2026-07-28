097510a8a41619a777805d7b6a9c35a321db0de8
Anas Al-Sweity <123195588+aalsweity@users.noreply.github.com>
2026-07-28 17:28:45 -0400

    Time: 0 ms (100.00%) | Memory: 8.4 MB (99.63%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,30 @@
+class Solution 
+{
+public:
+    int mySqrt(int x) 
+    {
+        
+        if(x == 0)
+        {
+            return 0;
+        }
+        int low = 1; // lowest possible value
+        int high = x; // highest value possible.
+        int result;
+        while(low <= high)
+        {
+            long int mid = low + (high - low)/2;
+            
+            if(mid*mid <= x) // change bounds to higher if number is equal or higher.
+            {
+                result = mid;
+                low = mid + 1;
+            }
+            else // change bounds ot lower otherwise
+            {
+                high = mid - 1;
+            }
+        }
+        return result;
+    }
+};
